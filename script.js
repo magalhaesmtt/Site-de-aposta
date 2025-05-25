50f0fc237a170006e7579a75bbda19916019ef60
magalhaesmtt <matheusmagalhaes2303@gmail.com>
2025-05-25 01:53:56 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Login efetuado com sucesso! (Simulação)');
+});
+
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Cadastro realizado com sucesso! (Simulação)');
+});
